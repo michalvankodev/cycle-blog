9eb062866f9ba51ef1ccba3b2885574a592c601f
Michal Vanko <mklakakilli@gmail.com>
2015-08-05 00:07:51 +0200

    css injecting into index.html

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -4,7 +4,7 @@ import browserSync from 'browser-sync';
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
-gulp.task('serve', ['less', 'browserify'], function(done) {
+gulp.task('serve', ['inject', 'browserify'], function(done) {
   browserSync({
     open: false,
     port: 9000,
