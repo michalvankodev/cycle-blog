18f995fb8888324f0363dcd744e404a1b626125b
Michal Vanko <mklakakilli@gmail.com>
2015-08-22 20:55:48 +0200

    updated dependencies and added header

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 import Cycle from '@cycle/core';
 import {h, makeDOMDriver} from '@cycle/dom';
+import {renderHeader} from './header/header';
 import {navigation} from './navigation';
 
 function view(state) {
-  let navigationItems = [
-    {href: 'home', name: 'Home'},
-    {href: 'articles', name: 'Articles'}
-  ];
 
   return Cycle.Rx.Observable.just(false).map(just =>
-    h('header', h('blog-navigation', {items: navigationItems}))
+    renderHeader()
   );
 }
 
---
+++ src/header/header.js
@@ -0,0 +1,23 @@
+/** @jsx hJSX */
+import {hJSX} from '@cycle/dom';
+
+let binary = Number(420).toString(2);
+let numberOfZeroes = 16 - binary.length;
+let zeroes = '0'.repeat(numberOfZeroes);
+const fourTwenty = zeroes + binary;
+
+let navigationItems = [
+  { href: 'home', name: 'Home'},
+  { href: 'articles', name: 'Articles'}
+];
+
+export function renderHeader() {
+
+  return (
+    <header>
+      <h1>Michal's blog</h1>
+      <div className='motto'>{fourTwenty}</div>
+      <blog-navigation items={navigationItems} />
+    </header>
+  );
+}
